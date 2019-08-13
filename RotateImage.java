442f470f00dddc307934c10f0111f951a48df704
lsk <lsk@LT-903512>
2019-08-13 09:55:44 +0300

    added RotateImage Java solution

---
+++ RotateImage.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        int middle = n/2;
+        
+        for (int i = 0; i< middle; i++){
+            for(int j = i; j< n-1-i; j++){
+                int rotateRowI = i;
+                int rotateColumnI = j;
+                
+                int itemToPut = matrix[rotateRowI][rotateColumnI];
+                
+                int step = 0;
+                int itemToBeReplaced;
+                
+                
+                do{
+                    itemToBeReplaced = matrix[rotateColumnI][n - 1 - rotateRowI];
+                    matrix[rotateColumnI][n - 1 - rotateRowI]=
+                        itemToPut;
+                    itemToPut = itemToBeReplaced;
+                    
+                    int oldRotateCol = rotateColumnI;
+                    rotateColumnI=n - 1 - rotateRowI;
+                    rotateRowI=oldRotateCol;
+                    step++;
+                }
+                while(step<4);
+            }
+        }
+    }
+}
