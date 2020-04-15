572dcf3bacab6455ca869e8a70011bb5f47bc4a9
lsk <lsk@LT-903512>
2020-04-15 16:43:01 +0300

    added link Taxicab_geometry

--- CJYouCanGoYourOwnWay.java
+++ CJYouCanGoYourOwnWay.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
+//https://en.wikipedia.org/wiki/Taxicab_geometry
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
